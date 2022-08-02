aa0bdaad2797e3f78c4764709cf5684c3c176647
Ryan Omasta <22267679+elementemerald@users.noreply.github.com>
2022-08-02 03:49:50 -0600

    Add GitHub, remove Discord widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,12 @@ const Index = () => {
         <img className="centerLogoInner" src="https://edge.cdn.emeraldsys.xyz/internal/assets/emeraldsys_new_nobg.png" />
       </div>
       <div className="centerProjects">
+        <a href="https://github.com/EmeraldSys">
+          <img style={{ height: "48px" }} src="https://edge.cdn.emeraldsys.xyz/internal/assets/logo-github-invert.png" />
+        </a>
+        <a href="https://gitlab.emeraldsys.xyz">
+          <img style={{ height: "48px" }} src="https://edge.cdn.emeraldsys.xyz/internal/assets/gitlab-logo-750.png" />
+        </a>
         <a href="https://acidity.emeraldsys.xyz">
           <img style={{ height: "48px" }} src="https://emeraldsys.github.io/images/products/concept2_transparent.png" />
         </a>
@@ -28,12 +34,6 @@ const Index = () => {
         <a href="https://edge.cdn.emeraldsys.xyz">
           <img style={{ filter: "invert(100%)" }} src="https://raw.githubusercontent.com/google/material-design-icons/master/png/file/cloud_download/materialiconsoutlined/48dp/1x/outline_cloud_download_black_48dp.png" />
         </a>
-        <a href="https://gitlab.emeraldsys.xyz">
-          <img style={{ height: "48px" }} src="https://edge.cdn.emeraldsys.xyz/internal/assets/gitlab-logo-750.png" />
-        </a>
-      </div>
-      <div className="centerDiscord">
-        <iframe src="https://canary.discord.com/widget?id=918034043868704808&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
       </div>
       <span className="mainFooter">
         &copy; 2022 EmeraldSys Media Ltd. All rights reserved. | <a style={{ color: "#ddd", textDecoration: "none" }} href="https://status.emeraldsys.xyz">Service Status</a>
