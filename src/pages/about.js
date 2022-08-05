a346897da9d149d225a8d2f865184d86e82270a7
Ryan Omasta <22267679+elementemerald@users.noreply.github.com>
2022-08-05 14:47:19 -0600

    Remove testing

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,30 +5,9 @@ import "../styles/about.css"
 import { w3cwebsocket as W3CWebSocket } from "websocket"
 
 const About = () => {
-    const [presence, setPresence] = React.useState({
-        "spotify":null,
-        "listening_to_spotify":false,
-        "kv":{
-            "website":"https://www.emeraldsys.xyz",
-            "pronouns":"he/him",
-            "birthday":"07/06/2004"
-        },
-        "discord_user":{
-            "username":"ryand",
-            "public_flags":256,
-            "id":"660292639412846621",
-            "discriminator":"4175",
-            "bot":false,
-            "avatar":"2643fb9c7d17ca57223563669b074a70"
-        },
-        "discord_status":"offline",
-        "activities":[],
-        "active_on_discord_web":false,
-        "active_on_discord_mobile":false,
-        "active_on_discord_desktop":false
-    });
+    const [presence, setPresence] = React.useState(null);
 
-    /* React.useEffect(() => {
+    React.useEffect(() => {
         let initWS = new W3CWebSocket("wss://api.lanyard.rest/socket");
 
         initWS.onopen = () => {
@@ -65,7 +44,7 @@ const About = () => {
         };
 
         return () => initWS.close();
-    }, []); */
+    }, []);
 
     return (
         <>
