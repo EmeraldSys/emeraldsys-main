cd72f0b873d76f562ac1e3334ada1d5f9118d354
Ryan Omasta <22267679+elementemerald@users.noreply.github.com>
2022-08-05 14:42:51 -0600

    Remove .DS_Store from everywhere

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,30 @@ import "../styles/about.css"
 import { w3cwebsocket as W3CWebSocket } from "websocket"
 
 const About = () => {
-    const [presence, setPresence] = React.useState(null);
+    const [presence, setPresence] = React.useState({
+        "spotify":null,
+        "listening_to_spotify":false,
+        "kv":{
+            "website":"https://www.emeraldsys.xyz",
+            "pronouns":"he/him",
+            "birthday":"07/06/2004"
+        },
+        "discord_user":{
+            "username":"ryand",
+            "public_flags":256,
+            "id":"660292639412846621",
+            "discriminator":"4175",
+            "bot":false,
+            "avatar":"2643fb9c7d17ca57223563669b074a70"
+        },
+        "discord_status":"offline",
+        "activities":[],
+        "active_on_discord_web":false,
+        "active_on_discord_mobile":false,
+        "active_on_discord_desktop":false
+    });
 
-    React.useEffect(() => {
+    /* React.useEffect(() => {
         let initWS = new W3CWebSocket("wss://api.lanyard.rest/socket");
 
         initWS.onopen = () => {
@@ -21,7 +42,7 @@ const About = () => {
 
                 if (json.op === 0)
                 {
-                    console.log(json);
+                    setPresence(json.d);
                 }
                 else if (json.op === 1)
                 {
@@ -44,7 +65,7 @@ const About = () => {
         };
 
         return () => initWS.close();
-    }, []);
+    }, []); */
 
     return (
         <>
