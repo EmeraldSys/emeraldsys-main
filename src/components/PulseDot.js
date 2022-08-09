84d2cfd0f93f607a587f67ec489e778363973175
Ryan Omasta <22267679+elementemerald@users.noreply.github.com>
2022-08-08 23:16:55 -0600

    Add components

---
+++ src/components/PulseDot.js
@@ -0,0 +1,13 @@
+import { keyframes } from "styled-components"
+
+export default keyframes`
+    0% {
+        transform: scale(.8);
+    }
+    50% {
+        transform: scale(1);
+    }
+    100% {
+        transform: scale(.8);
+    }
+`;
---
+++ src/components/PulseRing.js
@@ -0,0 +1,10 @@
+import { keyframes } from "styled-components"
+
+export default keyframes`
+    0% {
+        transform: scale(.33);
+    }
+    80%, 100% {
+        opacity: 0;
+    }
+`;
