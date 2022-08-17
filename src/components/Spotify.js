464e18b729f901a0e8dbfcf1d88ad878008d9d71
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-17 00:03:15 -0600

    Add song elapsed and length to Spotify

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -39,22 +39,51 @@ const SpotifyDetails = styled.div`
     }
 `;
 
-export default (props) => (
-    props.src && <SpotifyHolder>
-        <SpotifyAlbumHolder>
-            <SpotifyAlbumImage src={props.src.album_art_url} />
-            <SpotifyIndicator src="https://cdn-icons-png.flaticon.com/512/174/174872.png" />
-        </SpotifyAlbumHolder>
-        <SpotifyDetailHolder>
-            <SpotifyDetails>
-                <a href={`https://open.spotify.com/track/${props.src.track_id}`} target="_blank">{props.src.song}</a>
-            </SpotifyDetails>
-            <SpotifyDetails>
-                by {props.src.artist}
-            </SpotifyDetails>
-            <SpotifyDetails>
-                on {props.src.album}
-            </SpotifyDetails>
-        </SpotifyDetailHolder>
-    </SpotifyHolder>
-);
+const msToMinSeconds = millis => {
+    let minutes = Math.floor(millis / 60000);
+    let seconds = Number(((millis % 60000) / 1000).toFixed(0));
+    return seconds == 60 ? minutes + 1 + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+};
+
+export default (props) => {
+    const [current, setCurrent] = React.useState(new Date().getTime());
+    const [length, setLength] = React.useState(msToMinSeconds(props.src.timestamps.end - props.src.timestamps.start));
+    const [elapsed, setElapsed] = React.useState("");
+
+    React.useEffect(() => {
+        let interval = setInterval(() => {
+            setCurrent(new Date().getTime());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    React.useEffect(() => {
+        const spotify = props.src;
+        const timeElapsed = current - spotify.timestamps.start;
+        setElapsed(msToMinSeconds(timeElapsed));
+    }, [current]);
+
+    return (
+        props.src && <SpotifyHolder>
+            <SpotifyAlbumHolder>
+                <SpotifyAlbumImage src={props.src.album_art_url} />
+                <SpotifyIndicator src="https://cdn-icons-png.flaticon.com/512/174/174872.png" />
+            </SpotifyAlbumHolder>
+            <SpotifyDetailHolder>
+                <SpotifyDetails>
+                    <a href={`https://open.spotify.com/track/${props.src.track_id}`} target="_blank">{props.src.song}</a>
+                </SpotifyDetails>
+                <SpotifyDetails>
+                    by {props.src.artist}
+                </SpotifyDetails>
+                <SpotifyDetails>
+                    on {props.src.album}
+                </SpotifyDetails>
+                <SpotifyDetails>
+                    {elapsed} / {length}
+                </SpotifyDetails>
+            </SpotifyDetailHolder>
+        </SpotifyHolder>
+    );
+};
